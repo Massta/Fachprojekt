3eda914cfda8a16d83ff659d7a1369369bc6ad26
Julius Jacobsohn <Julius Jacobsohn@DESKTOP-VVMPAMJ>
2017-12-14 14:10:49 +0100

    Added forward propagation

--- FachprojektAufgabe5/Program.cs
+++ FachprojektAufgabe5/Program.cs
@@ -11,9 +11,13 @@ namespace FachprojektAufgabe5
 {
     class Program
     {
-        const string PATH_TEST_SMALL_GRAY = @"C:\Users\Julius Jacobsohn\Documents\Kaggle\Test_Small_Grayscale";
-        const string PATH_TRAIN_SMALL_GRAY = @"C:\Users\Julius Jacobsohn\Documents\Kaggle\Train_Small_Grayscale";
-        const string PATH_TRAIN_SMALL_GRAY_FILTER1 = @"C:\Users\Julius Jacobsohn\Documents\Kaggle\Train_Small_Grayscale_Filter1";
+        public const string PATH_TEST_SMALL_GRAY = @"C:\Users\Julius Jacobsohn\Documents\Kaggle\Test_Small_Grayscale";
+        public const string PATH_TRAIN_SMALL_GRAY = @"C:\Users\Julius Jacobsohn\Documents\Kaggle\Train_Small_Grayscale";
+        public const string PATH_TRAIN_SMALL_GRAY_FILTER1 = @"C:\Users\Julius Jacobsohn\Documents\Kaggle\Train_Small_Grayscale_Filter1";
+
+        public static int[][,] ImageData;
+        public static int[] Labels;
+        public const double LEARN_RATE = 0.005;
 
         public const int FILTER1_AMOUNT = 32;
         public const int FILTER1_SIZE = 2;
@@ -35,20 +39,35 @@ namespace FachprojektAufgabe5
         public static double[][,] Filters3Result;
         public static double[][] Filter3Weights;
 
-        static Random random = new Random();
+        public const int LAYER_HIDDEN_SIZE = 300;
+        public const int LAYER_HIDDEN_INPUT = 512;
+
+        public const int LAYER_OUTPUT_SIZE = 2;
+        public const int LAYER_OUTPUT_INPUT = 300;
+
+        public static double[,] HiddenWeights; //300, 512
+        public static double[] HiddenBias; //300
+        public static double[] HiddenOutputs; //300
+        public static double[] HiddenError; //300
+
+        public static double[,] OutputWeights; //2, 300
+        public static double[] OutputBias; //2
+        public static double[] OutputOutputs; //2
+        public static double[] OutputError; //2
 
         static void Main(string[] args)
         {
             //Load image data
+            #region Convolution Init
             string csvPath = Path.Combine(PATH_TRAIN_SMALL_GRAY, "Train.csv");
-            int[][,] imageData = ReadCsv(csvPath);
+            ImageData = Utilities.ReadCsv(csvPath, out Labels);
 
             //Load filters
             Filters1 = new double[FILTER1_AMOUNT][,];
             Filters1Result = new double[FILTER1_AMOUNT][,];
             for (int i = 0; i < FILTER1_AMOUNT; i++)
             {
-                Filters1[i] = GenerateWeightMatrix(FILTER1_SIZE, -0.5, 1);
+                Filters1[i] = Utilities.GenerateWeightMatrix(FILTER1_SIZE, -0.5, 1);
             }
 
             Filters2 = new double[FILTER2_AMOUNT][][,];
@@ -60,8 +79,8 @@ namespace FachprojektAufgabe5
                 Filter2Weights[i] = new double[FILTER1_AMOUNT];
                 for (int j = 0; j < FILTER1_AMOUNT; j++)
                 {
-                    Filters2[i][j] = GenerateWeightMatrix(FILTER2_SIZE, -0.5, 1);
-                    Filter2Weights[i][j] = GetRandomNumber(0, 2);
+                    Filters2[i][j] = Utilities.GenerateWeightMatrix(FILTER2_SIZE, -0.5, 1);
+                    Filter2Weights[i][j] = Utilities.GetRandomNumber(0, 2);
                 }
             }
 
@@ -74,17 +93,32 @@ namespace FachprojektAufgabe5
                 Filter3Weights[i] = new double[FILTER2_AMOUNT];
                 for (int j = 0; j < FILTER2_AMOUNT; j++)
                 {
-                    Filters3[i][j] = GenerateWeightMatrix(FILTER3_SIZE, -0.5, 1);
-                    Filter3Weights[i][j] = GetRandomNumber(0, 2);
+                    Filters3[i][j] = Utilities.GenerateWeightMatrix(FILTER3_SIZE, -0.5, 1);
+                    Filter3Weights[i][j] = Utilities.GetRandomNumber(0, 2);
                 }
             }
 
-            for (int imageIndex = 0; imageIndex < imageData.Count(); imageIndex++)
+            #endregion
+
+            #region FC Init
+            HiddenWeights = Utilities.GenerateWeightMatrix(LAYER_HIDDEN_SIZE, LAYER_HIDDEN_INPUT, -0.5, 0.5);
+            HiddenBias = new double[300];
+            HiddenOutputs = new double[300];
+            HiddenError = new double[300];
+
+            OutputWeights = Utilities.GenerateWeightMatrix(LAYER_OUTPUT_SIZE, LAYER_OUTPUT_INPUT, -0.5, 0.5);
+            OutputBias = new double[2];
+            OutputOutputs = new double[2];
+            OutputError = new double[2];
+            #endregion
+
+            for (int imageIndex = 0; imageIndex < ImageData.Count(); imageIndex++)
             {
-                var image = imageData[imageIndex];
+                var image = ImageData[imageIndex];
                 double[,] dst = new double[image.GetLength(0), image.GetLength(1)];
                 Array.Copy(image, dst, image.Length);
                 //PrintResult(dst, imageIndex, 0, -1);
+                #region Convolution
                 //Use filters 1
                 for (int f = 0; f < FILTER1_AMOUNT; f++)
                 {
@@ -100,7 +134,7 @@ namespace FachprojektAufgabe5
                                 + image[x + 1, y + 1] * filter[1, 1];
                         }
                     }
-                    PrintResult(Filters1Result[f], imageIndex, 1, f);
+                    Utilities.PrintResult(Filters1Result[f], imageIndex, 1, f);
                 }
                 //Use filters 2
                 for (int fti = 0; fti < FILTER2_AMOUNT; fti++) //Schleife um alle 16 Filter Tensoren
@@ -122,7 +156,7 @@ namespace FachprojektAufgabe5
                             }
                         }
                     }
-                    PrintResult(Filters2Result[fti], imageIndex, 2, fti);
+                    Utilities.PrintResult(Filters2Result[fti], imageIndex, 2, fti);
                 }
                 //Use filters 3
                 for (int fti = 0; fti < FILTER3_AMOUNT; fti++) //Schleife um alle 8 Filter Tensoren
@@ -144,103 +178,61 @@ namespace FachprojektAufgabe5
                             }
                         }
                     }
-                    PrintResult(Filters3Result[fti], imageIndex, 3, fti);
+                    Utilities.PrintResult(Filters3Result[fti], imageIndex, 3, fti);
                 }
 
+                #endregion
 
-            }
-        }
+                #region FC
 
-        private static void PrintResult(double[,] image, int imageIndex, int filterLevel, int filterIndex)
-        {
-            Bitmap grayscale = new Bitmap(image.GetLength(0), image.GetLength(0));
-            int x, y;
-
-
-            // Loop through the images pixels to reset color.
-            for (x = 0; x < image.GetLength(0); x++)
-            {
-                for (y = 0; y < image.GetLength(0); y++)
+                //<Forwardprop>
+                //Berechne outputs des hidden layers
+                for (int i = 0; i < LAYER_HIDDEN_SIZE; i++)
                 {
-                    int grayScale = Clamp((int)(image[x, y]), 0, 255);
-                    Color nc = Color.FromArgb(0, grayScale, grayScale, grayScale);
-                    grayscale.SetPixel(x, y, nc);
+                    double outputSum = 0;
+                    int counter = 0;
+                    for (int d = 0; d < FILTER3_AMOUNT; d++)
+                    {
+                        for (int x = 0; x < Filters3Result[d].GetLength(0); x++)
+                        {
+                            for (int y = 0; y < Filters3Result[d].GetLength(0); y++)
+                            {
+                                outputSum += HiddenWeights[i, counter] * ImageData[d][x, y];
+                                counter++;
+                            }
+                        }
+                    }
+                    outputSum += HiddenBias[i];
+                    HiddenOutputs[i] = Utilities.Sigmoid(outputSum);
                 }
-            }
-            grayscale.Save(Path.Combine(PATH_TRAIN_SMALL_GRAY_FILTER1, $"{imageIndex}.{filterLevel}.{filterIndex}.jpg"), ImageFormat.Jpeg);
-        }
 
-        private static int[][,] ReadCsv(string csvPath)
-        {
-            var lines = File.ReadAllLines(csvPath);
-            var takeLines = lines.OrderBy(l => l.Substring(2048, 3)).Take(100).ToArray();
-            int[][,] data = new int[takeLines.Count()][,];
-            for (int i = 0; i < data.Count(); i++)
-            {
-                var line = takeLines[i];
-                var lineData = line.Split(';').Take(4096);
-                var lineVector = lineData.Select(d => int.Parse(d)).ToArray();
-                data[i] = ConvertMatrix(lineVector, 64, 64);
-            }
-            return data;
-        }
-        private static int[,] ConvertMatrix(int[] flat, int m, int n)
-        {
-            int[,] ret = new int[m, n];
-            for (int i = 0; i < m; i++)
-            {
-                for (int j = 0; j < n; j++)
+                //Berechne outputs des output layers
+                for (int i = 0; i < LAYER_OUTPUT_SIZE; i++)
                 {
-                    ret[i, j] = flat[i * m + j];
+                    double outputSum = 0;
+                    for (int j = 0; j < LAYER_OUTPUT_INPUT; j++)
+                    {
+                        outputSum += OutputWeights[i, j] * HiddenOutputs[j];
+                    }
+                    outputSum += OutputBias[i];
+                    OutputOutputs[i] = Utilities.Sigmoid(outputSum);
                 }
-            }
-            return ret;
+                //</Forwardprop>
+                int biggestIndex = -1;
+                double biggestOutput = -1;
+                for (int i = 0; i < LAYER_OUTPUT_SIZE; i++)
+                {
+                    if (OutputOutputs[i] > biggestOutput)
+                    {
+                        biggestIndex = i;
+                        biggestOutput = OutputOutputs[i];
+                    }
+                }
 
-            //if (flat.Length != m * n)
-            //{
-            //    throw new ArgumentException("Invalid length");
-            //}
-            //int[,] ret = new int[m, n];
-            //// BlockCopy uses byte lengths: a double is 8 bytes
-            //Buffer.BlockCopy(flat, 0, ret, 0, flat.Length * sizeof(int));
-            //return ret;
-        }
-        private static double[,] GenerateWeightMatrix(int size, double min, double max)
-        {
-            double[,] weights = new double[size, size];
-            for (int i = 0; i < size; i++)
-            {
-                var tempWeights = GenerateWeightArray(size, min, max);
-                for (int j = 0; j < tempWeights.Count(); j++)
-                {
-                    weights[i, j] = tempWeights[j];
-                }
-            }
-            return weights;
-        }
-        private static double[] GenerateWeightArray(int size, double min, double max)
-        {
-            return GenerateWeights(size, min, max).ToArray();
-        }
-        private static IEnumerable<double> GenerateWeights(int amount, double min, double max)
-        {
-            object syncLock = new object();
-            for (int i = 0; i < amount; i++)
-            {
-                lock (syncLock)
-                {
-                    yield return (random.NextDouble() * (max - min)) + min;
-                }
-            }
-        }
+                Console.WriteLine($"Geraten: {biggestIndex} ({biggestOutput}) Ergebnis: {Labels[imageIndex]}");
+                #endregion
 
-        private static double GetRandomNumber(double min, double max)
-        {
-            return (random.NextDouble() * (max - min)) + min;
-        }
-        private static int Clamp(int value, int min, int max)
-        {
-            return (value < min) ? min : (value > max) ? max : value;
+            }
         }
     }
 }
---
+++ FachprojektAufgabe5/Utilities.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FachprojektAufgabe5
+{
+    public class Utilities
+    {
+
+        static Random random = new Random();
+
+        public static void PrintResult(double[,] image, int imageIndex, int filterLevel, int filterIndex)
+        {
+            Bitmap grayscale = new Bitmap(image.GetLength(0), image.GetLength(0));
+            int x, y;
+
+
+            // Loop through the images pixels to reset color.
+            for (x = 0; x < image.GetLength(0); x++)
+            {
+                for (y = 0; y < image.GetLength(0); y++)
+                {
+                    int grayScale = Clamp((int)(image[x, y]), 0, 255);
+                    Color nc = Color.FromArgb(0, grayScale, grayScale, grayScale);
+                    grayscale.SetPixel(x, y, nc);
+                }
+            }
+            grayscale.Save(Path.Combine(Program.PATH_TRAIN_SMALL_GRAY_FILTER1, $"{imageIndex}.{filterLevel}.{filterIndex}.jpg"), ImageFormat.Jpeg);
+        }
+
+        public static int[][,] ReadCsv(string csvPath, out int[] labels)
+        {
+            var lines = File.ReadAllLines(csvPath);
+            var takeLines = lines.OrderBy(l => l.Substring(2100, 3)).Take(100).ToArray();
+            int[][,] data = new int[takeLines.Count()][,];
+            labels = new int[takeLines.Count()];
+            for (int i = 0; i < data.Count(); i++)
+            {
+                var line = takeLines[i];
+                var lineData = line.Split(';');
+                labels[i] = lineData[4096] == "cat" ? 0 : 1;
+                var lineVector = lineData.Take(4096).Select(d => int.Parse(d)).ToArray();
+                data[i] = ConvertMatrix(lineVector, 64, 64);
+            }
+            return data;
+        }
+        public static int[,] ConvertMatrix(int[] flat, int m, int n)
+        {
+            int[,] ret = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    ret[i, j] = flat[i * m + j];
+                }
+            }
+            return ret;
+
+            //if (flat.Length != m * n)
+            //{
+            //    throw new ArgumentException("Invalid length");
+            //}
+            //int[,] ret = new int[m, n];
+            //// BlockCopy uses byte lengths: a double is 8 bytes
+            //Buffer.BlockCopy(flat, 0, ret, 0, flat.Length * sizeof(int));
+            //return ret;
+        }
+        public static double Sigmoid(double x)
+        {
+            double exp_val = Math.Exp(-7 * x);
+            return 1.0 / (1.0 + exp_val);
+        }
+
+        public static double SigmoidDerivative(double x)
+        {
+            return x * (1 - x);
+        }
+        public static double[,] GenerateWeightMatrix(int size, double min, double max)
+        {
+            return GenerateWeightMatrix(size, size, min, max);
+        }
+        public static double[,] GenerateWeightMatrix(int m, int n, double min, double max)
+        {
+            double[,] weights = new double[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                var tempWeights = GenerateWeightArray(n, min, max);
+                for (int j = 0; j < tempWeights.Count(); j++)
+                {
+                    weights[i, j] = tempWeights[j];
+                }
+            }
+            return weights;
+        }
+        public static double[] GenerateWeightArray(int size, double min, double max)
+        {
+            return GenerateWeights(size, min, max).ToArray();
+        }
+        public static IEnumerable<double> GenerateWeights(int amount, double min, double max)
+        {
+            object syncLock = new object();
+            for (int i = 0; i < amount; i++)
+            {
+                lock (syncLock)
+                {
+                    yield return (random.NextDouble() * (max - min)) + min;
+                }
+            }
+        }
+
+        public static double GetRandomNumber(double min, double max)
+        {
+            return (random.NextDouble() * (max - min)) + min;
+        }
+        public static int Clamp(int value, int min, int max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+    }
+}
