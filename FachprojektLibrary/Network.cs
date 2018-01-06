0351c87ad856d6f76958ea8f3b2aee04de680300
Julius Jacobsohn <nv@netzlab.de>
2018-01-06 12:35:11 +0100

    Added library project

---
+++ FachprojektLibrary/Network.cs
@@ -0,0 +1,46 @@
+﻿namespace FachprojektLibrary
+{
+    public class Network
+    {
+        public ILayer[] _layers { get; set; }
+        public Network(params ILayer[] layers)
+        {
+            _layers = layers;
+        }
+        public double[] GetOutputs(double[] inputs)
+        {
+            double[] oldOutputs = _layers[0].GetOutputs(inputs);
+            for (int i = 1; i < _layers.Length; i++)
+            {
+                oldOutputs = _layers[i].GetOutputs(oldOutputs);
+            }
+            return oldOutputs;
+        }
+
+        public void AdjustWeights(double label, double guess)
+        {
+            var topLayer = _layers[_layers.Length - 1];
+
+            double[] topError = new double[topLayer.NeuronAmount];
+            for (int i = 0; i < topLayer.NeuronAmount; i++)
+            {
+                double y = i == label ? 1 : 0;
+                double x = i == guess ? 1 : 0;
+                topError[i] = y - x;
+            }
+            topLayer.AdjustWeights(topError);
+            for (int i = _layers.Length - 2; i >= 0; i--)
+            {
+                var currentLayer = _layers[i];
+                var currentTopLayer = _layers[i + 1];
+
+                double[] currentError = new double[currentLayer.NeuronAmount];
+                for (int e = 0; e < currentLayer.NeuronAmount; e++)
+                {
+                    currentError[e] = currentTopLayer.GetWeightErrorSum(e);
+                }
+                currentLayer.AdjustWeights(currentError);
+            }
+        }
+    }
+}
---
+++ FachprojektLibrary/Neuron.cs
@@ -0,0 +1,66 @@
+﻿namespace FachprojektLibrary
+{
+    public class Neuron
+    {
+        public int _size;
+        public double[] _weights;
+        public double _bias;
+
+        private double _error;
+        private double _lastOutput;
+        private double[] _lastInputs;
+
+        public double _learnRate;
+
+        public bool UseReLu;
+
+        public Neuron(int size, bool useReLu = false, double learnRate = 0.01)
+        {
+            _learnRate = learnRate;
+            UseReLu = useReLu;
+            _size = size;
+            _weights = Utilities.GenerateWeights(size, -0.1, 0.1);
+        }
+
+        public double GetOutput(double[] inputs)
+        {
+            _lastInputs = inputs;
+            double sum = 0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                sum += _weights[i] * inputs[i];
+            }
+            sum += _bias;
+            if (UseReLu)
+            {
+                _lastOutput = Utilities.ReLu(sum);
+            }
+            else
+            {
+                _lastOutput = Utilities.Sigmoid(sum);
+            }
+            return _lastOutput;
+        }
+        public void AdjustWeights(double topError)
+        {
+            if (UseReLu)
+            {
+                _error = Utilities.ReLuDerivative(_lastOutput) * topError;
+            }
+            else
+            {
+                _error = Utilities.SigmoidDerivative(_lastOutput) * topError;
+            }
+            for (int i = 0; i < _size; i++)
+            {
+                _weights[i] += _learnRate * _error * _lastInputs[i];
+            }
+            _bias += _learnRate * _error;
+        }
+
+        public double GetWeightError(int i)
+        {
+            return _error * _weights[i];
+        }
+    }
+}
